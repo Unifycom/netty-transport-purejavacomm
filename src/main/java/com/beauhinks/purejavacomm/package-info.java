1911fa8717faff216baf9bca132f4720d841dbad
Unifycom <unifycom@outlook.com>
2023-06-03 12:06:02 +0800

    clean project

--- src/main/java/com/beauhinks/purejavacomm/package-info.java
+++
@@ -1,6 +0,0 @@
-
-
-/**
- * A serial and parallel port communication transport based on <a href="http://www.sparetimelabs.com/purejavacomm/purejavacomm.php">PureJavaComm</a>.
- */
-package com.beauhinks.purejavacomm;
